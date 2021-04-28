d7c93b49844b4dd219475f3d425147285cfa523f
OlexandrFedorkoDev <Fedorko2323@gmail.com>
2021-04-28 14:27:13 +0300

    function random box, size and position

--- index.js
+++ index.js
@@ -14,22 +14,32 @@ function handleBoxClick(event) {
 }
 
 function startGame() {
-    console.log('start');
     $game.style.backgroundColor = '#fff';
     $start.classList.add('hide');
 
     renderBox()
 }
 function renderBox() {
+    console.log(getRandom(30, 100))
     $game.innerHTML = ' ';
-    var box = document.createElement('div');
-    box.style.height = box.style.width = '50px';
+    let box = document.createElement('div');
+    let boxsize = getRandom(30, 100)
+    let gameSize = $game.getBoundingClientRect();
+    let maxTop = gameSize.height - boxsize;
+    let maxLeft = gameSize.width - boxsize;
+    console.log(gameSize)
+
+    box.style.height = box.style.width = boxsize + 'px';
     box.style.position = 'absolute';
     box.style.backgroundColor = '#000';
-    box.style.top = '50px';
-    box.style.left = '70px';
+    box.style.top = getRandom(0, maxTop) + 'px';
+    box.style.left = getRandom(0, maxLeft) + 'px';
     box.style.cursor = 'pointer';
     box.setAttribute('data-box', 'true');
 
     $game.insertAdjacentElement("afterbegin", box);
+}
+function getRandom(min, max) {
+
+    return Math.floor(Math.random() * (max - min) + min)
 }

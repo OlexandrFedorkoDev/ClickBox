401366b2f4c55153dbcdc218f02d20e50a5c07bc
OlexandrFedorkoDev <Fedorko2323@gmail.com>
2021-04-28 16:11:41 +0300

    timer

--- index.js
+++ index.js
@@ -1,45 +1,68 @@
-let $start = document.getElementById('start');
-let $game = document.getElementById('game');
-let score = 0;
+let $start = document.querySelector('#start')
+let $game = document.querySelector('#game')
+let $time = document.querySelector('#time')
 
-$start.addEventListener('click', startGame);
-$game.addEventListener('click', handleBoxClick);
+let score = 0
+let isGameStarted = false
 
-function handleBoxClick(event) {
-    if (event.target.dataset.box) {
-        score++
-        renderBox()
+$start.addEventListener('click', startGame)
+$game.addEventListener('click', handleBoxClick)
 
-    }
-}
 
 function startGame() {
-    $game.style.backgroundColor = '#fff';
-    $start.classList.add('hide');
+    isGameStarted = true
+    $game.style.backgroundColor = '#fff'
+    $start.classList.add('hide')
+
+    let interval = setInterval(function () {
+        let time = parseFloat($time.textContent)
+
+        if (time <= 0) {
+            clearInterval(interval)
+            endGame()
+        } else {
+            $time.textContent = (time - 0.1).toFixed(1)
+        }
+    }, 100)
 
     renderBox()
 }
-function renderBox() {
-    console.log(getRandom(30, 100))
-    $game.innerHTML = ' ';
-    let box = document.createElement('div');
-    let boxsize = getRandom(30, 100)
-    let gameSize = $game.getBoundingClientRect();
-    let maxTop = gameSize.height - boxsize;
-    let maxLeft = gameSize.width - boxsize;
-    console.log(gameSize)
 
-    box.style.height = box.style.width = boxsize + 'px';
-    box.style.position = 'absolute';
-    box.style.backgroundColor = '#000';
-    box.style.top = getRandom(0, maxTop) + 'px';
-    box.style.left = getRandom(0, maxLeft) + 'px';
-    box.style.cursor = 'pointer';
-    box.setAttribute('data-box', 'true');
-
-    $game.insertAdjacentElement("afterbegin", box);
+function endGame() {
+    isGameStarted = false
 }
-function getRandom(min, max) {
 
+function handleBoxClick(event) {
+    if (!isGameStarted) {
+        return
+    }
+
+    if (event.target.dataset.box) {
+        score++
+        renderBox()
+    }
+}
+
+function renderBox() {
+    $game.innerHTML = ''
+    let box = document.createElement('div')
+    let boxSize = getRandom(30, 100)
+    let gameSize = $game.getBoundingClientRect()
+    let maxTop = gameSize.height - boxSize
+    let maxLeft = gameSize.width - boxSize
+
+    box.style.height = box.style.width = boxSize + 'px'
+    box.style.position = 'absolute'
+    box.style.backgroundColor = '#000'
+    box.style.top = getRandom(0, maxTop) + 'px'
+    box.style.left = getRandom(0, maxLeft) + 'px'
+    box.style.cursor = 'pointer'
+    box.setAttribute('data-box', 'true')
+
+    $game.insertAdjacentElement('afterbegin', box)
+
+}
+
+function getRandom(min, max) {
     return Math.floor(Math.random() * (max - min) + min)
 }

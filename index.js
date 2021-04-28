4e9c2e05431284c7873a9271ea786f063eb0e4e8
OlexandrFedorkoDev <Fedorko2323@gmail.com>
2021-04-28 13:34:25 +0300

    function create box

--- index.js
+++ index.js
@@ -1,13 +1,35 @@
-let $start = document.getElementById('start')
-let $game = document.getElementById('game')
+let $start = document.getElementById('start');
+let $game = document.getElementById('game');
+let score = 0;
 
-$start.addEventListener('click', startGame)
+$start.addEventListener('click', startGame);
+$game.addEventListener('click', handleBoxClick);
+
+function handleBoxClick(event) {
+    if (event.target.dataset.box) {
+        score++
+        renderBox()
+
+    }
+}
 
 function startGame() {
-    console.log('start')
-    $game.style.backgroundColor = "#fff"
+    console.log('start');
+    $game.style.backgroundColor = '#fff';
     $start.classList.add('hide');
+
+    renderBox()
 }
 function renderBox() {
+    $game.innerHTML = ' ';
+    var box = document.createElement('div');
+    box.style.height = box.style.width = '50px';
+    box.style.position = 'absolute';
+    box.style.backgroundColor = '#000';
+    box.style.top = '50px';
+    box.style.left = '70px';
+    box.style.cursor = 'pointer';
+    box.setAttribute('data-box', 'true');
 
+    $game.insertAdjacentElement("afterbegin", box);
 }
